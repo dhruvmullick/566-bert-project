668f4e106b07f14f0d447a97b36dc594b4d3f43b
Dhruv Mullick <dmullick95@gmail.com>
2021-02-23 23:13:20 +0530

    Update to Ridge Regression

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import RidgeCV
 import numpy as np
 
 '''TODO: See how regularisation working. is it only over training set or not? 
@@ -6,15 +6,18 @@ After training, will we use the best regularisation or not. For MC.
 Check the training objective.
 '''
 
-class EegLogisticRegression:
+class EegLinearRegression:
 
     def __init__(self, folds=5, regularisation_bound=(0.1, 100), max_iterations=100):
         '''need to see why logspace dependency not working'''
-        c_values = np.logspace(1 / regularisation_bound[1], 1 / regularisation_bound[0], num=500)
-        self.model = LogisticRegressionCV(cv=folds, penalty='l2', Cs=c_values, max_iter=max_iterations)
+        alphas = np.logspace(regularisation_bound[0], regularisation_bound[1], num=500)
+        # Uses K-Fold Regularisation here.
+        self.model = RidgeCV(cv=folds, alphas=alphas, max_iter=max_iterations)
 
     def train(self, x, y):
         self.model.fit(x, y)
 
+    # Change this to use Nested CV approach / (Monte Carlo + CV) approach for getting the alpha for from training set
+    # and then making prediction for test set.
     def test(self, x):
         return self.model.predict(x)
