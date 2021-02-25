86bd50d46140f1a9d5fc6160d080ceb98fa325dc
brij1823 <brijpatel1823@gmail.com>
2021-02-26 02:55:39 +0530

    True_word_sourcer and EEG_Feature_Extractor features completed

--- src/eeg_feature_extractor.py
+++ src/eeg_feature_extractor.py
@@ -1,5 +1,18 @@
-"""Given a list of words, return corresponding eeg representations"""
-
-
-def get_eeg_data_by_word(words):
-    pass
+def eeg_feature_extractor(word_list):
+    main_list = []
+    for participant in range(1):   # change to number of participant
+        single_participant = {}
+        mat_file = scipy.io.loadmat('proc/S01.mat')    #Convert to loop by adding all the mat files
+        raw_file = scipy.io.loadmat('S01_dataset.mat') #Convert to loop by adding all the dataset files.     
+        #print(raw_file['raw']['trial'][0][0][0][0].shape)
+        for single_word in word_list:
+            word,index = single_word.split('$')
+            index = int(index)
+            onset, offset = int(mat_file['proc']['trl'][0][0][index-1][0]),int(mat_file['proc']['trl'][0][0][index-1][1])
+            filtered_data = raw_file['raw']['trial'][0][0][0][0][:,onset:offset+1]
+            try:
+                single_participant[word].append(filtered_data)
+            except:
+                single_participant[word]=[filtered_data]
+        main_list.append(single_participant)
+    return main_list
--- src/true_word_sourcer.py
+++ src/true_word_sourcer.py
@@ -1,6 +1,15 @@
-def get_words_for_training():
-    pass
-
-
-def get_words_for_test():
-    pass
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import scipy.io
+def true_word_sourcer(proportion):
+    proportion/=100
+    df = pd.read_csv('dataset.csv')
+    words = list(df['Word'])
+    for entry in range(len(words)):words[entry] = words[entry]+'$'+str(entry+1)
+    random.shuffle(words)
+    partition_point = int(proportion*len(words))
+    train_set,testing_set = words[:partition_point],words[partition_point:]
+    #print(train_set)
+    return [train_set,testing_set]
