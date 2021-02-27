addfe41e43a3eced28da2409d1d2fbb6d1e6e797
Dhruv Mullick <dmullick95@gmail.com>
2021-02-27 17:33:33 +0530

    Add nested cross validation to the ridge regression model

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -1,13 +1,17 @@
-from sklearn.linear_model import RidgeCV
+import enum
+
 import numpy as np
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import KFold
 
 '''
 Ridge Regression for applying on EEG data. 
-Includes method abstractions for training, testing and an evaluator method call.  
+To be accessed through evaluation() method call. This will abstract the training, testing process as per the evaluation method selected.  
 '''
 
 
 class EegLinearRegression:
+    cv_types = enum.Enum('nested_cv')
 
     # Initialise the ridge regression model.
     def __init__(self, folds=5, regularisation_bound=(0.1, 200), max_iterations=500):
@@ -17,13 +21,27 @@ class EegLinearRegression:
         self.model = RidgeCV(cv=folds, alphas=alphas, max_iter=max_iterations, scoring="neg_mean_squared_error")
 
     # Fit the LR model on the input data.
-    def train(self, x, y):
-        self.model.fit(x, y)
+    def __train(self, x, y):
+        return self.model.fit(x, y)
 
     # Test the LR model to get the predicted score for the given input data.
-    def test(self, x):
-        return self.model.predict(x)
+    def __test(self, x, y):
+        return self.model.score(x, y)
 
-    # Evaluate the LR model by giving an accuracy value.
-    def evaluation(self, x, y):
-        pass
+    # Calculate the nested cv score.
+    def __nested_cv_score(self, x, y, outer_folds=5):
+        outer_cv = KFold(outer_folds, shuffle=True)
+        scores = []
+        for train_data_ind, test_data_ind in outer_cv.split(x, y):
+            x_train, x_test = x[train_data_ind, :], x[test_data_ind, :]
+            y_train, y_test = y[train_data_ind], y[test_data_ind]
+            # Trains RidgeCV with cross validation.
+            self.__train(x_train, y_train)
+            best_score_for_fold = self.__test(x_test, y_test)
+            scores += [best_score_for_fold]
+        return np.average(np.array(scores))
+
+    # Evaluate the LR model by giving an accuracy value. Will accept type of evaluation as well.
+    def evaluation(self, x, y, cv_type=cv_types.nested_cv):
+        if cv_type == self.cv_types.nested_cv:
+            return self.__nested_cv_score(self, x, y)
