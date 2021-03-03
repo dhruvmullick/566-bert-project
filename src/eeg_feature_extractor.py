ab80c40a877354e2b7f5eaab163c7fd20ea30881
brij1823 <brijpatel1823@gmail.com>
2021-03-03 16:18:39 +0530

    Sentence wise code modified in eeg_feature_extractor and true_word_sourcer

--- src/eeg_feature_extractor.py
+++ src/eeg_feature_extractor.py
@@ -1,18 +1,30 @@
-def get_eeg_data_for_words(word_list):
+def eeg_feature_extractor(sentence_list):
     main_list = []
     for participant in range(1):   # change to number of participant
-        single_participant = {}
+        #single_participant = {}
         mat_file = scipy.io.loadmat('proc/S01.mat')    #Convert to loop by adding all the mat files
         raw_file = scipy.io.loadmat('S01_dataset.mat') #Convert to loop by adding all the dataset files.     
         #print(raw_file['raw']['trial'][0][0][0][0].shape)
-        for single_word in word_list:
-            word,index = single_word.split('$')
-            index = int(index)
-            onset, offset = int(mat_file['proc']['trl'][0][0][index-1][0]),int(mat_file['proc']['trl'][0][0][index-1][1])
-            filtered_data = raw_file['raw']['trial'][0][0][0][0][:,onset:offset+1]
-            try:
-                single_participant[word].append(filtered_data)
-            except:
-                single_participant[word]=[filtered_data]
-        main_list.append(single_participant)
+        matrix = []
+        for single_sentence in sentence_list:
+            word_list = single_sentence.split(' ')
+            row  =  []
+            for single_word in word_list:
+                word,index = single_word.split('$')
+                index = int(index)
+                onset, offset = int(mat_file['proc']['trl'][0][0][index-1][0]),int(mat_file['proc']['trl'][0][0][index-1][1])
+                filtered_data = raw_file['raw']['trial'][0][0][0][0][:,onset-1:offset]
+#                 try:
+#                     single_participant[word].append(filtered_data)
+#                 except:
+#                     single_participant[word]=[filtered_data]
+                row.append(filtered_data)
+            matrix.append(row)
+                
+        #main_list.append(single_participant)
+        main_list.append(matrix)
     return main_list
+                
+            
+data = eeg_feature_extractor(train_set)
+    
--- src/true_word_sourcer.py
+++ src/true_word_sourcer.py
@@ -3,15 +3,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import scipy.io
+import re
+def get_sentence():
+    with open('mod_sentence.txt') as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    content = ''.join(content)
+    content = content.lower()
+    sentence = ""
+    for character in range(len(content)):
+        if content[character].isalpha() or content[character] == " " or content[character] == "." or (content[character] == "'" and content[character-1]!=" " and content[character+1]!=" "):
+            sentence+=content[character]
 
 
-def get_words(proportion=70):
-    proportion /= 100
+    sentence = sentence.replace('waistcoatpocket','waistcoat pocket')
+    sentence = sentence.replace("'",' ')
+    sentence = sentence.split('.')
+    return sentence
+
+    
+def true_word_sourcer(proportion):
+    proportion/=100
     df = pd.read_csv('dataset.csv')
     words = list(df['Word'])
-    for entry in range(len(words)): words[entry] = words[entry] + '$' + str(entry + 1)
-    random.shuffle(words)
-    partition_point = int(proportion * len(words))
-    train_set, testing_set = words[:partition_point], words[partition_point:]
-    # print(train_set)
-    return [train_set, testing_set]
+    for entry in range(len(words)):words[entry] = words[entry]+'$'+str(entry+1)
+    reference_sentence = get_sentence()
+    final_sentence = []
+    starting_index = 0
+    for single_sentence in reference_sentence:
+        reference_sentence_list = single_sentence.split(' ')
+        idol_sentence_list = words[starting_index:starting_index+len(reference_sentence_list)]
+        starting_index += len(reference_sentence_list)
+        if len(' '.join(idol_sentence_list)):final_sentence.append(' '.join(idol_sentence_list))
+    random.shuffle(final_sentence)
+    for i in final_sentence:print(i,'\n')
+    partition_point = int(proportion*len(final_sentence))
+    train_set,testing_set = final_sentence[:partition_point],final_sentence[partition_point:]
+    return [train_set,testing_set]
+
+data = true_word_sourcer(90)
+train_set = data[0]
+test_set = data[1]
