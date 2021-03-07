346dc0a8dff93439e764d114defafa4229946a07
brij1823 <brijpatel1823@gmail.com>
2021-03-07 17:52:56 +0530

    Working prototype for all sentence

--- src/eeg_feature_extractor.py
+++ src/eeg_feature_extractor.py
@@ -5,10 +5,8 @@ import numpy as np
 def extract_eeg_feature_for_sentences(sentence_list):
     main_list = []
     for participant in range(1):   # change to number of participant
-        #single_participant = {}
         mat_file = scipy.io.loadmat('S01.mat')    #Convert to loop by adding all the mat files
         raw_file = scipy.io.loadmat('S01_dataset.mat') #Convert to loop by adding all the dataset files.     
-        #print(raw_file['raw']['trial'][0][0][0][0].shape)
         matrix = []
         for single_sentence in sentence_list:
             word_list = single_sentence.split(' ')
@@ -18,17 +16,12 @@ def extract_eeg_feature_for_sentences(sentence_list):
                 index = int(index)
                 onset, offset = int(mat_file['proc']['trl'][0][0][index-1][0]),int(mat_file['proc']['trl'][0][0][index-1][1])
                 filtered_data = raw_file['raw']['trial'][0][0][0][0][:,onset-1:offset]
-#                 try:
-#                     single_participant[word].append(filtered_data)
-#                 except:
-#                     single_participant[word]=[filtered_data]
 
                 
                 filtered_data = filtered_data.mean(axis = 1)
                 row.append(np.array(filtered_data))
             matrix.append(np.array(row))
                 
-        #main_list.append(single_participant)
         main_list.append(np.array(matrix))
     return np.array(main_list)
 
--- src/main.py
+++ src/main.py
@@ -9,19 +9,36 @@ MAX_BERT_SIZE = 512 - 2
 train_set_sentences, testing_set_sentences = get_sentences_by_proportion()
 sentences = train_set_sentences + testing_set_sentences
 
+
+
 eeg_representations = extract_eeg_feature_for_sentences(sentences)
 eeg_representations_averaged = average_eeg_over_participants(eeg_representations)
-eeg_representations_truncated = np.zeros(eeg_representations_averaged.shape)
+eeg_representations_truncated = []
 
 for i in range(eeg_representations_averaged.shape[0]):
-    eeg_representations_truncated[i] = eeg_representations_averaged[i][:min(len(eeg_representations_averaged[i]), MAX_BERT_SIZE)]
+    eeg_representations_truncated.append(eeg_representations_averaged[i][:min(len(eeg_representations_averaged[i]), MAX_BERT_SIZE)])
+eeg_representations_truncated = np.array(eeg_representations_truncated)
 
-bert_features_for_sentences = np.array(len(sentences))
+bert_features_for_sentences = []
+
 for i in range(len(sentences)):
     features = get_bert_features_for_layer(sentences[i], 12)
     features = features[1:min(MAX_BERT_SIZE, eeg_representations_truncated[i].shape[0]) + 1]
-    bert_features_for_sentences[i] = features
-
+    bert_features_for_sentences.append(features)
+bert_features_for_sentences = np.array(bert_features_for_sentences)
 ridge_regression = EegLinearRegression()
-score = ridge_regression.evaluate(eeg_representations_truncated.ravel(), bert_features_for_sentences.ravel())
+
+flatten_eeg_representations_truncated = []
+for i in range(len(eeg_representations_truncated)):
+  flatten_eeg_representations_truncated.extend(eeg_representations_truncated[i])
+
+
+flatten_bert_features_for_sentences = []
+for i in range(len(bert_features_for_sentences)):
+  flatten_bert_features_for_sentences.extend(bert_features_for_sentences[i])
+
+flatten_eeg_representations_truncated = np.array(flatten_eeg_representations_truncated)
+flatten_bert_features_for_sentences = np.array(flatten_bert_features_for_sentences)
+
+score = ridge_regression.evaluate(flatten_eeg_representations_truncated, flatten_bert_features_for_sentences)
 print(score)
