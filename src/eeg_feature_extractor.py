62f278e367dd740fe84380419eb62d63cf384e25
brij1823 <brijpatel1823@gmail.com>
2021-04-01 00:00:55 +0530

    EEG Sliding Window all participants

--- src/eeg_feature_extractor.py
+++ src/eeg_feature_extractor.py
@@ -55,8 +55,10 @@ def extract_eeg_feature_for_sentences(sentence_list):
 
 def extract_eeg_window_feature_for_sentences(sentence_list):
     main_list = []
-    for participant in range(1,15):   # change to number of participant
-        if participant == 5:continue   # lol
+    not_present = [47,46,33,31,29,28]
+    #red_word = [36,35,34,32,30,24,26,27]
+    for participant in range(1,42):   # change to number of participant
+        if participant in not_present :continue
         print('Participant : ',participant,' in progress')
         try:
           number = ''
@@ -70,20 +72,33 @@ def extract_eeg_window_feature_for_sentences(sentence_list):
           for single_sentence in sentence_list:
               word_list = single_sentence.split(' ')
               row  =  []
+              flag = 0  # to check which sentence is read properly
+              counter = 1
               for single_word in word_list:
                   word,index = single_word.split('$')
                   index = int(index)
-                  onset, offset = int(mat_file['proc']['trl'][0][0][index-1][0]),int(mat_file['proc']['trl'][0][0][index-1][1])
-                  filtered_data = raw_file['raw']['trial'][0][0][0][0][:,onset-1:offset]  # [62,651]                  
-                  concat_vec = []
-                  for item in [275, 300, 325, 350, 375, 400]:  # Decoding word and category-specific spatiotemporal representations from MEG and EEG
-                      concat_vec.extend(filtered_data[:,item:item+25].mean(axis = 1))
-                  row.append(np.array(concat_vec))
+                  try:
+                      onset, offset = int(mat_file['proc']['trl'][0][0][index-1][0]),int(mat_file['proc']['trl'][0][0][index-1][1])
+                      filtered_data = raw_file['raw']['trial'][0][0][0][0][:,onset-1:offset]  # [62,651]                  
+                      concat_vec = []
+                      for item in [275, 300, 325, 350, 375, 400]:  # Decoding word and category-specific spatiotemporal representations from MEG and EEG
+                        concat_vec.extend(filtered_data[:,item:item+25].mean(axis = 1))
+                      row.append(np.array(concat_vec))
+                      flag = counter
+                      counter+=1
+                  except:
+                      counter+=1
+                      continue
+
+                 
+                  
                   # if np.isnan(np.array(concat_vec)).any() or np.isinf(np.array(concat_vec)).any():
                   #   print("badness")
               #print(len(row[0]))
               matrix.append(np.array(row))
                   
+          if flag!=0:print(len(matrix[flag][0]),len(matrix))
+          else:print("Vocabulary Issue")
           main_list.append(np.array(matrix))
         except:
           print('Entry : ',participant,' Something Went Wrong')
