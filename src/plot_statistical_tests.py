c60578c08fe8a393232e05fd123073a77dbf3b7a
Dhruv Mullick <dmullick95@gmail.com>
2021-04-04 22:34:58 +0530

    Add code for plotting statistical tests, data for single layer

---
+++ src/plot_statistical_tests.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Plot results of statistical tests
+'''
+
+values = []
+file = open("data/single_layer_stat_test", "r")
+for line in file.readlines():
+    values += [float(line.strip('\n'))]
+file.close()
+
+fig, ax = plt.subplots(1, 1)
+ax.hist(values, bins=20)
+ax.set_title("Single Layer Permutation Scores")
+ax.set_xlabel('MSE Scores')
+ax.set_ylabel('No. of Outcomes')
+plt.show()
