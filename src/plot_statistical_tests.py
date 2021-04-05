112f1573af84104836ba7e92131ccb1d68ebb6e0
Dhruv Mullick <dmullick95@gmail.com>
2021-04-06 01:02:51 +0530

    Multi layer stat test data

--- src/plot_statistical_tests.py
+++ src/plot_statistical_tests.py
@@ -6,14 +6,14 @@ Plot results of statistical tests
 '''
 
 values = []
-file = open("data/single_layer_stat_test", "r")
+file = open("data/multi_layer_stat_test", "r")
 for line in file.readlines():
     values += [float(line.strip('\n'))]
 file.close()
 
 fig, ax = plt.subplots(1, 1)
 ax.hist(values, bins=20)
-ax.set_title("Single Layer Permutation Scores")
+ax.set_title("Multi Layer Permutation Scores")
 ax.set_xlabel('MSE Scores')
 ax.set_ylabel('No. of Outcomes')
 plt.show()
