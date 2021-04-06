f1bef79cdc159b4b195330eb820eff7576fe5338
Dhruv Mullick <dmullick95@gmail.com>
2021-04-06 12:41:39 +0530

    Add observed score and legend to stat test

--- src/plot_statistical_tests.py
+++ src/plot_statistical_tests.py
@@ -6,14 +6,17 @@ Plot results of statistical tests
 '''
 
 values = []
-file = open("data/multi_layer_stat_test", "r")
+file = open("data/single_layer_stat_test", "r")
 for line in file.readlines():
     values += [float(line.strip('\n'))]
 file.close()
 
 fig, ax = plt.subplots(1, 1)
-ax.hist(values, bins=20)
-ax.set_title("Multi Layer Permutation Scores")
+ax.hist(values, bins=20, label='Permutation Scores')
+ax.set_title("Single Layer Permutation Scores")
 ax.set_xlabel('MSE Scores')
 ax.set_ylabel('No. of Outcomes')
+# plt.axvline(x=0.267662, color='red', linestyle='--', label='Observed Score')
+plt.axvline(x=0.164762, color='red', linestyle='--', label='Observed Score')
+plt.legend()
 plt.show()
